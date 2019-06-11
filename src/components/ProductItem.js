2c680b1b0c5b46b839793e6c8306d5c00f2a11a3
Ng Chun Shien <ngchunshien@Ngs-MacBook-Pro.local>
2019-06-12 00:54:07 +0800

    create base design

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,11 +1,53 @@
 import React from 'react';
 
-function ProductItem() {
-    return (
-      <div>
-        abc
-      </div>
-    );
+class ProductItem extends React.Component  {
+    render() {
+        return (
+            <div style={{backgroundColor: 'white', margin: 10}}>
+                <div>
+                    <img className='productImage' src={this.props.productImage} />
+                </div>
+                <hr style={{backgroundColor: '#f0f0f0', height: 1, border: 0, margin: 0}}/>
+                <div style={{padding: 10}}>
+                    <div className='productName'>{this.props.productName}</div>
+                    <div className='productDescription'>{this.props.productDescription}</div>
+                    <div className='productPrice'>{this.props.productPrice}</div>
+                </div>
+                <style jsx='true'>{`                                    
+                    .productImage {
+                        width: 90%;
+                        max-width: 400px;
+                        height: 350px;
+                        object-fit: cover;
+                        padding: 5% 5px;
+                    }
+
+                    .productName, .productDescription, .productPrice {                        
+                        text-align: left;
+                    }                
+                    
+                    .productName{
+                        color: #323232;
+                        font-size: 14px;
+                    }
+
+                    .productDescription{
+                        color: #b1b1b1;
+                        font-size: 13px;
+                    }
+
+                    .productPrice{
+                        color: #000;
+                        font-weight: 600;
+                        font-size: 14px;
+                    }
+                `}</style>
+            </div>
+        );
+    }
   }
-  
+  const divStyle = {
+    margin: '40px',
+    border: '5px solid pink'
+  };
   export default ProductItem;
--- src/routes/Product.js
+++ src/routes/Product.js
@@ -12,8 +12,31 @@ class Product extends React.Component {
                 <div className='title'>All Products</div>
                 <div className='sub-title'>{this.state.productTotal} Products</div>
                 <div className='sub-page'>8 per page</div>
-                <ProductItem />
-                <style jsx>{`
+                <div className='productItem'>
+                    <ProductItem 
+                        productImage={'https://dummyimage.com/318x336.png/5fa2dd/ffffff'}
+                        productName={'name'}
+                        productDescription={'desc'}
+                        productPrice={'16.99'}
+                    />
+                </div>
+                <div className='productItem'>
+                    <ProductItem 
+                        productImage={'http://dummyimage.com/317x312.png/ff4444/ffffff'}
+                        productName={'name'}
+                        productDescription={'desc'}
+                        productPrice={'16.99'}
+                    />
+                </div>  
+                <div className='productItem'>
+                    <ProductItem 
+                        productImage={'http://dummyimage.com/350x318.png/dddddd/000000'}
+                        productName={'name'}
+                        productDescription={'desc'}
+                        productPrice={'16.99'}
+                    />
+                </div>              
+                <style jsx ='true'>{`
                 .title {                    
                     color: #323232;
                     font-size: 16px;
@@ -22,7 +45,7 @@ class Product extends React.Component {
                     text-align: left;
                 }        
                 .sub-title, .sub-page {                    
-                    font-size: 12px;
+                    font-size: 14px;
                     width: 50%;
                     float: left;                    
                 }
@@ -33,6 +56,29 @@ class Product extends React.Component {
                 .sub-page{                   
                     color: #323232;
                     text-align: right;
+                }
+                
+                .productItem{
+                    float: left;   
+                }
+
+                /* mobile only */
+                @media only screen and (min-width: 0px) and (max-width: 575px){
+                    .productItem{
+                        width: 100%;
+                    }
+                }
+                /* tablet */
+                @media only screen and (min-width: 576px) and (max-width: 991px){
+                    .productItem{
+                        width: 50%;
+                    }
+                }
+                /* desktop */
+                @media only screen and (min-width: 992px) {
+                    .productItem{
+                        width: 450px;
+                    }
                 }
             `}</style>
             </div>
