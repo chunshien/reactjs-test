b5eefdd52a1a3b1e1986c788e96fadfae64280be
Ng Chun Shien <ngchunshien@Ngs-MacBook-Pro.local>
2019-06-16 12:22:22 +0800

    simple router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Product from './routes/Product.js';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">      
-      <Product />
-    </div>
+    <Router>
+      <div className="App">            
+        <Route exact path="/" component={Product} />
+        <Route path="/product" component={Product} />
+      </div>
+    </Router>
   );
 }
 
